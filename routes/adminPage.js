9e32734df47b0fccd01db88168de362e360af2e4
ox01220122 <ox0122@gmail.com>
2023-07-12 15:28:38 +0900

    관리자 페이지 patch router 코드추가

--- routes/adminPage.js
+++ routes/adminPage.js
@@ -3,6 +3,8 @@ const controller = require('../controller/adminPage');
 const router = express.Router();
 
 //관리자 페이지
-// GET localhost:8000/admin
+// GET localhost:8000/adminpage
 router.get('/adminpage', controller.CgetAdminPage);
+// POST localhost:8000/adminpage
+router.post('/adminpage', controller.CpatchAdminPage);
 module.exports = router;
