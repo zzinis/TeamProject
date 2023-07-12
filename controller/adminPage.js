400ba35f247ef4638c95ce3fc7de1febd2a09675
ox01220122 <ox0122@gmail.com>
2023-07-12 16:23:20 +0900

    수정된 행의 데이터를 가져오기

--- controller/adminPage.js
+++ controller/adminPage.js
@@ -11,15 +11,19 @@ exports.CgetAdminPage = (req, res) => {
 };
 
 //PATCH (관리자 페이지에 문의 글 댓글 달기)
-exports.CpatchAdminPage = (req, res) => {
+exports.CpatchAdminPage = async (req, res) => {
     const ask_id = req.body.ask_id;
     const manager_msg = req.body.comment;
     console.log('ask_id ', ask_id);
     console.log('manager_msg ', manager_msg);
 
     Ask.update({ manager_msg: manager_msg }, { where: { ask_id: ask_id } })
-        .then((result) => {
-            res.send(result);
+        .then(() => {
+            // 수정된 행의 데이터를 조회
+            return Ask.findByPk(ask_id);
+        })
+        .then((updatedAsk) => {
+            res.send(updatedAsk);
         })
         .catch((error) => {
             console.error('댓글 수정에 실패했습니다:', error);
