30f71708db6efec9bae084e032b7c0b33d10d130
ox01220122 <ox0122@gmail.com>
2023-07-12 17:01:59 +0900

    주석 추가

--- routes/adminLogin.js
+++ routes/adminLogin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const controller = require('../controller/adminLogin');
 const router = express.Router();
 
-// POST localhost:8000/admin
+// POST localhost:8000/adminLogin
 router.post('/adminLogin', controller.CpostAdminLogin);
 
 module.exports = router;
